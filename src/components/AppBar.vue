f7d2051655b2f262271e593ca3744d1d11555413
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-09-21 20:59:53 +0530

    [Akshay]:ADD:App Bar Component

---
+++ src/components/AppBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="tidiv">
+        <a class="md-title">Welcome To Quantity Measurement </a>
+  </div>
+</template>
+
+<script>
+  export default {
+   name: 'AppBar'
+}
+</script>
+
+<style scoped>
+.tidiv{
+  background-color:#446DFF;
+  text-align: center;
+  padding-top: 2%;
+  padding-bottom: 2%;
+}
+  .md-title{
+    font-size:xx-large;
+    color: white;    
+}
+</style>
